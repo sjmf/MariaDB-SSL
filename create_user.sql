de6f43b8318bf5ecdb12bfb5e4dc6a6395681635
Samantha Finnigan <1038320+sjmf@users.noreply.github.com>
2023-04-16 21:59:15 +0100

    Add instructional comment

--- create_user.sql
+++ create_user.sql
@@ -1,3 +1,4 @@
+-- Update the names for your database, user and password as needed:
 CREATE SCHEMA arc_iasc;
 CREATE USER 'arc_iasc'@'%' IDENTIFIED BY '1234' REQUIRE SSL;
 GRANT ALL PRIVILEGES ON arc_iasc.* TO 'arc_iasc'@'%';
